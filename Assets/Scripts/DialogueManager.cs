8e38c508ba11af1107c69677b29b1e88a34f604f
Tobias Tobak <tobiasfunction@protonmail.com>
2024-01-28 14:05:25 -0500

    Adds processing for word consistency

--- Assets/Scripts/DialogueManager.cs
+++ Assets/Scripts/DialogueManager.cs
@@ -1,19 +1,15 @@
 // adapted from https://github.com/shapedbyrainstudios/ink-dialogue-system/tree/7-dialogue-audio-implemented
+
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-public class DialogueManager: MonoBehaviour
-{
-
+public class DialogueManager: MonoBehaviour {
     [Header("Audio")]
     [SerializeField] private DialogueAudioInfoSO defaultAudioInfo;
-    [SerializeField] private DialogueAudioInfoSO[] audioInfos;
     public AudioClip[] dialogueTypingSoundClips;
     private DialogueAudioInfoSO currentAudioInfo;
-    private Dictionary < string, DialogueAudioInfoSO > audioInfoDictionary;
     public AudioSource audioSource;
-
 
     private void PlayDialogueSound(int currentDisplayedCharacterCount, char currentCharacter)
     {
@@ -21,28 +17,43 @@ public class DialogueManager: MonoBehaviour
         {
             currentAudioInfo = defaultAudioInfo;
         }
+
         // set variables for the below based on our config
         AudioClip soundClip = null;
         int frequencyLevel = currentAudioInfo.frequencyLevel;
         float minPitch = currentAudioInfo.minPitch;
         float maxPitch = currentAudioInfo.maxPitch;
         bool stopAudioSource = currentAudioInfo.stopAudioSource;
-        
-        //sound clip
-        int randomIndex = Random.Range(0, dialogueTypingSoundClips.Length);
-        soundClip = dialogueTypingSoundClips[randomIndex];
-        //pitch
-        audioSource.pitch = Random.Range(minPitch, maxPitch);
+
+        int hashCode = currentCharacter.GetHashCode();
+        // sound clip
+
+        int predictableIndex = Mathf.Abs(hashCode) % dialogueTypingSoundClips.Length;
+        soundClip = dialogueTypingSoundClips[predictableIndex];
+        // pitch
+        int minPitchInt = (int)(minPitch * 100);
+        int maxPitchInt = (int)(maxPitch * 100);
+        int pitchRangeInt = maxPitchInt - minPitchInt;
+        // cannot divide by 0, so if there is no range then skip the selection
+        if (pitchRangeInt != 0)
+        {
+            int predictablePitchInt = (hashCode % pitchRangeInt) + minPitchInt;
+            float predictablePitch = predictablePitchInt / 100 f;
+            audioSource.pitch = predictablePitch;
+        } else {
+            audioSource.pitch = minPitch;
+        }
 
         // play sound
         audioSource.PlayOneShot(soundClip);
     }
 
-/* -------------------------------------------------------------------------- */
-/*     EVERYTHING BELOW HERE (EXCEPT FOR THE FINAL BRACKET) IS FOR TESTING    */
-/* -------------------------------------------------------------------------- */
+    /* -------------------------------------------------------------------------- */
+    /*     EVERYTHING BELOW HERE (EXCEPT FOR THE FINAL BRACKET) IS FOR TESTING    */
+    /* -------------------------------------------------------------------------- */
 
-    string text = "Hello World";
+    // string text = "Hello World Hello World";
+    string text = "よく出来ましたね！";
     public Button button;
 
     // Start is called before the first frame update
@@ -60,14 +71,17 @@ public class DialogueManager: MonoBehaviour
     private IEnumerator DialogueMiddleman()
     {
         int idx = 0;
-        foreach (char c in text)
+        foreach(char c in text)
         {
-            yield return new WaitForSeconds(0.15f);
-            if (c != ' ')
+            yield
+            return new WaitForSeconds(0.15 f);
+            if (c == ' ')
             {
+                idx = 0;
+            } else {
                 PlayDialogueSound(idx, c);
+                idx++;
             }
-        idx ++;
         }
     }
 }
